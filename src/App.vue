73ea818e96c96d8634cd934836827b3961c4a1a2
yunyub-1664303 <yunyubaii@gmail.com>
2020-11-22 12:35:53 -0600

    added python scripts to build data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      Amazon Categories Tree
+    </h1>
+    <Tree/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tree from './components/Tree.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tree
   }
 }
 </script>
