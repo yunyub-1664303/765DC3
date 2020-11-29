8ad84a004ba77493c7dfc205a040cc3b131f46c3
yunyub-1664303 <yunyubaii@gmail.com>
2020-11-29 00:55:57 -0600

    changed to d3 tree

--- data/build_data.py
+++ data/build_data.py
@@ -2,6 +2,11 @@ import json
 from ast import literal_eval
 import csv
 
+src_file = 'all-nodes.csv'
+out_file = 'all-data.json'
+# src_file = 'PetSupplies.csv'
+# out_file = 'pet-data.json'
+
 class Node:
   def __init__(self, id: int, name: str, sub_cnt: int, children_ids: [int]):
     self.id = id
@@ -10,17 +15,20 @@ class Node:
     self.children_ids = children_ids
     
   def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.id == other.id
-        else:
-            return False
+    if isinstance(other, self.__class__):
+        return self.id == other.id
+    else:
+        return False
 
   def __str__(self):
-     return json.dumps(self.__dict__)
+    return json.dumps(self.__dict__)
+    
+  def to_json(self):
+    return json.loads(json.dumps(self.__dict__))
 
 # construct a list of all nodes in it
 nodes = []
-with open('all-nodes.csv') as file:
+with open(src_file) as file:
   reader = csv.DictReader(file)
   for row in reader:
     new_node = Node(row['id'], row['name'], row['subtreeProductCount'], literal_eval(row['children']))
@@ -29,10 +37,22 @@ with open('all-nodes.csv') as file:
 def get_height(node):
   if len(node.children_ids) == 0:
     return 0
-  return 1 + max(get_height(nodes[id]) for id in node.children_ids)
+  return 1 + max(nodes[id].height for id in node.children_ids)
 
 # compute the height of each node
-for node in nodes:
+# construct children array for each node
+for i in range(len(nodes) - 1, -1, -1):
+  node = nodes[i]
   node.height = get_height(node)
+  children = []
+  for kid_id in node.children_ids:
+    children.append(nodes[kid_id].to_json())
+  node._children = children
 
-print(nodes[29240])
+json_root = nodes[0].to_json()
+res = {"name": json_root['name'], "subcnt": json_root['sub_cnt'], "children": json_root['_children']}
+# for node in nodes:
+#   json_nodes.append(node.to_json())
+
+with open(out_file, 'w') as outfile:
+  json.dump(res, outfile)
--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,55 +1,233 @@
 <template>
   <div>
-    <div>
-      <tree id="tree" :data="tree" layoutType="circular"></tree>
-    </div>
+    <body>
+      <!-- <tree id="tree" :data="tree" :duration="1" @change="showBranch"></tree> -->
+      {{ drawTree(this.treeData) }}
+    </body>
   </div>
 </template>
 
 <script>
-import {tree} from 'vued3tree'
+// import {tree} from 'vued3tree'
+import data from '../../data/all-data'
+// import data from '../../data/pet-data'
+import * as d3 from 'd3';
 
 export default {
+  props: ['on'],
   components: {
-    tree
+    // tree
   },
   data() {
     return {
-      tree: {
-        name: "father",
-        children:[{
-          name: "son1",
-          children:[ {name: "grandson"}, {name: "grandson2"}], 
-          cnt: 50
-        },{
-          name: "son2",
-          children:[ {name: "grandson3"}, {name: "grandson4"}], 
-          cnt: 50,
-        }],
-        cnt: 100
-      }
+      treeData: data,
     }
   }, 
+  methods: {
+    showBranch(element) {
+      console.log(element)
+      element.data.children = element.data._children
+    }, 
+    drawTree(treeData) {
+      // clear previous tree
+       d3.select('svg').remove();
+      // ************** Generate the tree diagram	 *****************
+      root = treeData;
+      var margin = {top: 20, right: 20, bottom: 20, left: 200},
+        width = 1440,
+        height = 30 * root.children.length;
+        
+      var i = 0,
+        duration = 10,
+        root;
+
+      var tree = d3.layout.tree()
+        .size([height, width]);
+        // .size([10*height, width])
+        // .separation(function separation(a, b) { return a.parent == b.parent ? 2 : 1; });
+
+      var diagonal = d3.svg.diagonal()
+        .projection(function(d) { return [d.y, d.x]; });
+
+      var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // .attr("transform", "translate(" + (margin.left + width/2) + "," + margin.top + ")");
+
+      
+      root.x0 = height / 2;
+      root.y0 = 0;
+      update(root);
+
+      d3.select(self.frameElement).style("height", "500px");
+
+      function update(source) {
+
+        // Compute the new tree layout.
+        var nodes = tree.nodes(root).reverse(),
+          links = tree.links(nodes);
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+        // Update the nodes…
+        var node = svg.selectAll("g.node")
+          .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+        // Enter any new nodes at the parent's previous position.
+        var nodeEnter = node.enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function() { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+          .on("click", click);
+
+        nodeEnter.append("circle")
+          .attr("r", 1e-6)
+          .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+        nodeEnter.append("text")
+          .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
+          .attr("dy", ".35em")
+          .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+          .text(function(d) { return d.name; })
+          .style("fill-opacity", 1e-6);
+
+        // Transition nodes to their new position.
+        var nodeUpdate = node.transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+        nodeUpdate.select("circle")
+          .attr("r", 10)
+          .style("fill", function(d) { 
+            if ((d._children == null && d.children.length == 0) || (d.children == null && d._children.length == 0)) {
+              return "#b2b2b2";
+            }
+            return d._children ? "lightsteelblue" : "#034f84"; 
+          });
+
+        nodeUpdate.select("text")
+          .style("fill-opacity", 1);
+
+        // Transition exiting nodes to the parent's new position.
+        var nodeExit = node.exit().transition()
+          .duration(duration)
+          .attr("transform", function() { return "translate(" + source.y + "," + source.x + ")"; })
+          .remove();
+
+        nodeExit.select("circle")
+          .attr("r", 1e-6);
+
+        nodeExit.select("text")
+          .style("fill-opacity", 1e-6);
+
+        // Update the links…
+        var link = svg.selectAll("path.link")
+          .data(links, function(d) { return d.target.id; })
+          ;
+
+        // Enter any new links at the parent's previous position.
+        link.enter().insert("path", "g")
+          .attr("class", "link")
+          .attr("d", function() {
+          var o = {x: source.x0, y: source.y0};
+          return diagonal({source: o, target: o});
+          })
+          .attr("fill", "none")
+          .attr("stroke", "#555")
+          .attr("stroke-opacity", 0.4)
+          .attr("stroke-width", 1.5);
+
+        // Transition links to their new position.
+        link.transition()
+          .duration(duration)
+          .attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link.exit().transition()
+          .duration(duration)
+          .attr("d", function() {
+          var o = {x: source.x, y: source.y};
+          return diagonal({source: o, target: o});
+          })
+          .remove();
+
+        // Stash the old positions for transition.
+        nodes.forEach(function(d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+        });
+      }
+
+      // Toggle children on click.
+      function click(d) {
+        if ((d._children != null && d._children.length == 0) || (d.children != null && d.children.length == 0)) {
+          alert('no subcategories under ' + d.name)
+          return;
+        }
+        if (d.children) {
+        d._children = d.children;
+        d.children = null;
+        } else {
+        d.children = d._children;
+        d._children = null;
+        }
+        update(d);
+      }
+      
+    }
+  },
 }
 </script>
 
 <style scoped>
 /* .node circle {
+  cursor: pointer;
+  fill: #fff;
+  stroke: steelblue;
+  stroke-width: 1.5px;
+}
+
+.node text {
+  font-size: 11px;
+}
+
+path.link {
+  fill: none;
+  stroke: #ccc;
+  stroke-width: 1.5px;
+} */
+
+.node {
+		cursor: pointer;
+	}
+
+.node circle {
   fill: #fff;
   stroke: steelblue;
   stroke-width: 3px;
 }
 
-.node text { font: 12px sans-serif; }
+.node text {
+  font: 12px sans-serif;
+}
 
 .link {
   fill: none;
   stroke: #ccc;
   stroke-width: 2px;
-} */
-#tree {
-  max-width: 500px;
-  max-height: 500px;
-  margin: auto;
+}
+div#container {
+  height: 400px;
+  width: 400px;
+  border:2px solid #000;
+  overflow: scroll;
+ }
+svg#sky {
+  height: 100px;
+  width: 1100px;
+  border:1px dotted #ccc;
+  background-color: #ccc;
 }
 </style>
